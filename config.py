058d289a5145645feb8733b5f499209c12aa4c70
GalacticGlum <nerftactic09@gmail.com>
2018-03-05 22:03:46 -0500

    Rewrite project files.

--- config.py
+++ config.py
@@ -1,10 +1,25 @@
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-DEBUG = True
+# Create dummy secrey key so we can use sessions
+SECRET_KEY = '123456790'
+
+# Create in-memory database
+DATABASE_FILE = 'sample_db.sqlite'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
 SQLALCHEMY_ECHO = True
-BCRYPT_LOG_ROUNDS = 10
-USER_SEND_PASSWORD_CHANGED_EMAIL = False
-USER_SEND_REGISTERED_EMAIL = False
-USER_SEND_USERNAME_CHANGED_EMAIL = False
-USER_ENABLE_CONFIRM_EMAIL = False
-#USER_LOGIN_URL = '/'
-USER_LOGIN_TEMPLATE = 'home.html'
+
+# Flask-Security config
+SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
+SECURITY_PASSWORD_SALT = "ATGUOHAELKiubahiughaerGOJAEGj"
+
+# Flask-Security URLs, overridden because they don't put a / at the end
+SECURITY_LOGIN_URL = "/login/"
+SECURITY_LOGOUT_URL = "/logout/"
+SECURITY_REGISTER_URL = "/register/"
+
+SECURITY_POST_LOGIN_VIEW = "/"
+SECURITY_POST_LOGOUT_VIEW = "/"
+SECURITY_POST_REGISTER_VIEW = "/"
+
+# Flask-Security features
+SECURITY_REGISTERABLE = True
+SECURITY_SEND_REGISTER_EMAIL = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- grader/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, DateTimeField
-from wtforms.validators import DataRequired, Email
-
-class AddContestForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    start_time = DateTimeField('start_time', validators=[DataRequired()])
-    end_time = DateTimeField('end_time', validators=[DataRequired()])
