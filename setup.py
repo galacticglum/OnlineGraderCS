961b1c17a4e9bfe0f075716dc71e6a2eff76c6c9
GalacticGlum <nerftactic09@gmail.com>
2018-03-12 23:57:46 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -23,29 +23,5 @@ with application.app_context():
         password=encrypt_password('admin'),
         roles=[user_role, super_user_role]
     )
-
-    first_names = [
-        'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella', 'Charlie', 'Sophie', 'Mia',
-        'Jacob', 'Thomas', 'Emily', 'Lily', 'Ava', 'Isla', 'Alfie', 'Olivia', 'Jessica',
-        'Riley', 'William', 'James', 'Geoffrey', 'Lisa', 'Benjamin', 'Stacey', 'Lucy'
-    ]
-    last_names = [
-        'Brown', 'Smith', 'Patel', 'Jones', 'Williams', 'Johnson', 'Taylor', 'Thomas',
-        'Roberts', 'Khan', 'Lewis', 'Jackson', 'Clarke', 'James', 'Phillips', 'Wilson',
-        'Ali', 'Mason', 'Mitchell', 'Rose', 'Davis', 'Davies', 'Rodriguez', 'Cox', 'Alexander'
-    ]
-
-    for i in range(len(first_names)):
-        tmp_email = first_names[i].lower() + "." + \
-            last_names[i].lower() + "@example.com"
-        tmp_pass = ''.join(random.choice(
-            string.ascii_lowercase + string.digits) for i in range(10))
-        user_datastore.create_user(
-            first_name=first_names[i],
-            last_name=last_names[i],
-            email=tmp_email,
-            password=encrypt_password(tmp_pass),
-            roles=[user_role, ]
-        )
-        
+    
     db.session.commit()
