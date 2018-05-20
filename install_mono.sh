481dd6b5beb823532bfd56c69ad7ed7c0cec8aeb
GalacticGlum <nerftactic09@gmail.com>
2018-05-20 10:15:15 -0400

    Fixed install_mono.sh

--- install_mono.sh
+++ install_mono.sh
@@ -1,11 +1,6 @@
-sudo mkdir -p /tmp/mono_dependencies
-sudo cd /tmp/mono_dependencies
-sudo wget http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/Packages/l/libpng15-1.5.30-3.fc28.x86_64.rpm
-sudo yum install -y libpng15-1.5.30-3.fc28.x86_64.rpm
-sudo yum install yum-utils
+wget http://dl.fedoraproject.org/pub/fedora/linux/releases/26/Everything/x86_64/os/Packages/l/libpng15-1.5.28-2.fc26.x86_64.rpm
+sudo yum install -y libpng15-1.5.28-2.fc26.x86_64.rpm
+rm libpng15-1.5.28-2.fc26.x86_64.rpm
 sudo rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 sudo yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
-sudo yum clean all
-sudo yum makecache
-sudo yum install mono-devel -y
-sudo rm -rf /tmp/mono_dependencies
+sudo yum install -y mono
