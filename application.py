189e4c35186bcd6a2b7a4972915cbd60d5547e6a
GalacticGlum <nerftactic09@gmail.com>
2018-05-27 13:25:54 -0400

    Staging

--- application.py
+++ application.py
@@ -783,4 +783,5 @@ if __name__ == '__main__':
     context = (os.path.join(application.instance_path, 'dev.cert'), 
         os.path.join(application.instance_path, 'dev_cert.key'))
 
-    application.run(debug=True, ssl_context=context)
+    # application.run(debug=True, ssl_context=context)
+    application.run(debug=True)
