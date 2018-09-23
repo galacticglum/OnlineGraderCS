36ffa5a9d5c9b446ce4c3e03ebc4c0dde0485b96
GalacticGlum <nerftactic09@gmail.com>
2018-09-23 13:18:58 -0400

    First pass on the problem page (fetching from the API and displaying)

--- backend/grader/__init__.py
+++ backend/grader/__init__.py
@@ -9,10 +9,13 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 # Initialize flask
 application = Flask(__name__, instance_relative_config=True,
     instance_path=os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'instance')))
+
+CORS(application)
 
 application.config.from_pyfile('config_local.py')
 
--- frontend/src/components/pages/Problem.js
+++ frontend/src/components/pages/Problem.js
@@ -44,14 +44,26 @@ class Problem extends Component {
     componentWillMount() {
         const { id } = this.props.match.params;
 
-        let intId = parseInt(id);
-        if(intId != parseFloat(id) || intId <= 0) {
-            this.props.history.replace('/404');
-        }
+        fetch('http://localhost:5000/api/problem/' + id)
+            .then(res => {
+                if(!res.ok) {
+                    throw Error(res.statusText);
+                }
+
+                return res.json();
+            })
+            .then(json => this.setState({problemData: json})).catch(() => {
+                this.props.history.replace('/404');
+            });
     }
 
     render() {
-        const { id } = this.props.match.params;
+        // Don't render the component
+        // unless there is an actual problem
+        // to render.
+        if (!this.state.problemData) {
+            return null;
+        }
 
         return (
             <div>
@@ -59,12 +71,12 @@ class Problem extends Component {
                     <BreadcrumbNavigation>
                         <BreadcrumbItem><a href="#">Problems</a></BreadcrumbItem>
                         <BreadcrumbItem><a href="#">CCC</a></BreadcrumbItem>
-                        <BreadcrumbItem active>CCC '15 S2 - Jerseys</BreadcrumbItem>
+                        <BreadcrumbItem active>{this.state.problemData.problem.title}</BreadcrumbItem>
                     </BreadcrumbNavigation>
 
                     <div className="container py-3">
                         <Col md="12" className="px-0">
-                            <h2 className="problem-title">CCC '15 S2 - Jerseys</h2>     
+                            <h2 className="problem-title">{this.state.problemData.problem.title}</h2>     
                             {/* <div className="d-flex flex-row"> 
                                 <h2 className="problem-title">CCC '15 S2 - Jerseys</h2>
                                 <Badge className="ml-auto align-self-end" color="success">Easy</Badge>
@@ -75,75 +87,29 @@ class Problem extends Component {
                         <Row className="px-0">
                             <Col md="9">
                                 <div className="problem-description">
-                                    <p><strong>Canadian Computing Competition: 2015 Stage 1, Senior #2</strong></p>
-                                    <p>
-                                        A school team is trying to assign jerseys numbered 1, 2, 3, ..., J to student athletes. The size of
-                                        jersey is either small (<code>S</code>), medium (<code>M</code>), or large (<code>L</code>).
-                                    </p>
-
-                                    <p>
-                                        Each athlete has requested a specific jersey number and a preferred size. The athletes will not be satisfied with
-                                        a jersey that is the wrong number or that is smaller than their preferred size. They will be satisfied with a 
-                                        jersey that is their preferred size or larger as long as it is the right number. Two students cannot be given the
-                                        same jersey number.
-                                    </p>
-
-                                    <p>
-                                        Your task is to determine the maximum number of requests that can be satisfied.
-                                    </p>
+                                    {this.state.problemData.problem.description}
                                 </div>
            
-                                <div className="py-1 problem-constraints">
+                                <div className="pt-3 problem-constraints">
                                     <h5>Constraints</h5>
                                     <hr className="mt-0" />
-                                    <p>
-                                        1 ≤ j ≤ J
-                                    </p>
-                                    <p>
-                                        1 ≤ a ≤ A
-                                    </p>
+                                    {this.state.problemData.problem.constraints}
                                 </div>
 
-                                <div className="py-1 problem-input-spec">
+                                <div className="pt-3 problem-input-spec">
                                     <h5>Input Specification</h5>
                                     <hr className="mt-0" />
-                                    <p>
-                                        The first line of input is the integer J which is the number of jerseys.
-                                    </p>
-                                    <p>
-                                        The second line of input is the integer A which is the number of athletes.
-                                    </p>
-                                    <p>
-                                        The next J lines are each the character <code>S</code>, <code>M</code>, or <code>L</code>. Line j gives the
-                                        size of the jersey j.
-                                    </p>
-                                    <p>
-                                        The last A lines are each the character <code>S</code>, <code>M</code>, or <code>L</code> followed by a space, followed
-                                        by an integer. Line a gives the requested size and jersey number for athlete a where the athletes are numbered 
-                                        1, 2, 3, ..., A.
-                                    </p>
-                                    <p>
-                                        For 50% of the test cases, 1 ≤ J ≤ 10^3  and 1 ≤ A ≤ 10^3.
-                                    </p>
-                                    <p>
-                                        For the remaining 50% of the test cases, 1 ≤ J ≤ 10^6  and 1 ≤ A ≤ 10^6.
-                                    </p>
+                                    {this.state.problemData.problem.input_spec}
                                 </div>
 
-                                <div className="py-1 problem-output-spec">
+                                <div className="pt-3 problem-output-spec">
                                     <h5>Output Specification</h5>
                                     <hr className="mt-0" />
-                                    <p>
-                                        One day you woke up, finding yourself back in 1992. 
-                                        It would seem that the gypsy wife of the wizened old monk 
-                                        whose voodoo shop you smash up every day after school has cast a spell on you.
-                                    </p>
+                                    {this.state.problemData.problem.output_spec}
                                 </div>
 
                             </Col>
-                            <Col md="3" className="order-first order-md-2">
-                                {/* <p className="blog-post-meta">{props.post.date} by <a href="#">{props.post.user}</a></p> */}
-                                
+                            <Col md="3" className="order-first order-md-2">    
                                 <Button color="info" className="mb-0 btn-submit-solution">
                                     Submit Solution
                                 </Button>
@@ -164,9 +130,6 @@ class Problem extends Component {
                                     <a href="#" className="muted-link">Best Submissions</a>
                                 </div>
                                 <hr className="my-2" />
-                                <div className="text-muted">
-                                    Tags
-                                </div>
                                 <TagList tags={this.state.tags} />
                             </Col>
                         </Row>
