d0c5086f715923d5b858f27544c56119ecbd0c2b
GalacticGlum <nerftactic09@gmail.com>
2018-09-29 11:42:55 -0400

    Added server-side validation

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -6,7 +6,8 @@ import {
     FormGroup,
     Label,
     Input,
-    FormText
+    FormText,
+    FormFeedback
 } from 'reactstrap';
 
 class RegisterForm extends Component {
@@ -16,7 +17,9 @@ class RegisterForm extends Component {
             username: '',
             email: '',
             password: '',
-            passwordConfirmation: ''
+            passwordConfirmation: '',
+            errors: {},
+            isLoading: false
         }
 
         this.onChange = this.onChange.bind(this);
@@ -29,26 +32,46 @@ class RegisterForm extends Component {
 
     onSubmit(e) {
         e.preventDefault();
-        this.props.userSignupRequest(this.state);
+
+        this.setState({ isLoading: true });
+        this.props.userSignupRequest(this.state)
+            .then(() => {})
+            .catch((res) => {
+                this.setState({errors: res.response.data, isLoading: false})
+            });
     }
 
     render() {
+        const { errors } = this.state;
+
         return (
             <Form onSubmit={this.onSubmit}>
                 <FormGroup>
                     <Label for="usernameInput">Username</Label>
                     <Input type="text" name="username" id="usernameInput"
-                        value={this.state.username} onChange={this.onChange} />
+                        value={this.state.username} onChange={this.onChange} 
+                        invalid={(errors.parameter_info && errors.parameter_info.username) && true} />
+
+                    {(errors.parameter_info && errors.parameter_info.username) && 
+                        <FormFeedback>{errors.parameter_info.username}</FormFeedback>}
                 </FormGroup>
                 <FormGroup>
                     <Label for="emailInput">Email</Label>
                     <Input type="email" name="email" id="emailInput"
-                        value={this.state.email} onChange={this.onChange} />
+                        value={this.state.email} onChange={this.onChange}
+                        invalid={(errors.parameter_info && errors.parameter_info.email) && true} />
+                        
+                    {(errors.parameter_info && errors.parameter_info.email) && 
+                        <FormFeedback>{errors.parameter_info.email}</FormFeedback>}
                 </FormGroup>
                 <FormGroup>
                     <Label for="passwordInput">Password</Label>
                     <Input type="password" name="password" id="passwordInput"
-                        value={this.state.password} onChange={this.onChange} />
+                        value={this.state.password} onChange={this.onChange}
+                        invalid={(errors.parameter_info && errors.parameter_info.password) && true} />
+
+                    {(errors.parameter_info && errors.parameter_info.password) && 
+                        <FormFeedback>{errors.parameter_info.password}</FormFeedback>}
                 </FormGroup>
                 <FormGroup>
                     <Label for="confirmPasswordInput">Confirm Password</Label>
@@ -56,7 +79,7 @@ class RegisterForm extends Component {
                         value={this.state.passwordConfirmation} onChange={this.onChange} />
                 </FormGroup>
                 <FormGroup>
-                    <Button color="primary">
+                    <Button color="primary" disabled={this.state.isLoading}>
                         Sign up
                     </Button>
                 </FormGroup>
